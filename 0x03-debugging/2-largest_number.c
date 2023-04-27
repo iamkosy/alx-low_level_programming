037d25f77293ab47a71a3a40180c147056e27773
ANI TREASURE <treasure4real4real@gmail.com>
2023-04-27 10:18:02 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,7 +17,7 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	}
 	else if (a > b && c > a)
-	}
+	{
 		largest = c;
 	}
 	else if (b > c)
@@ -29,5 +29,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-return (largest);
+	return (largest);
 }
